ec9a3ae91f156c217ad486bfb79b26b2733b6f40
Sayali <saylikumbhar31@gmail.com>
2020-08-13 19:49:57 +0530

    First COmmit

---
+++ src/Employee.java
@@ -0,0 +1,26 @@
+
+public class Employee {
+	
+	void m1()
+	{
+		
+	}
+	
+	
+	
+public static void main(String[] args) {
+	System.out.println("Hello");	
+	
+}
+
+}
+
+class emp extends Employee{
+	
+	protected void m1()
+	{
+		
+	}
+	
+	
+}
